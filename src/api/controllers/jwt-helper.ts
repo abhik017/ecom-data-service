dad1048dc3ca6a25c47ccd4fdd553245f45b9bf0
Abhik Mehta <abhikmehta@usthaan.in>
2021-06-29 10:08:44 +0530

    fixes

--- src/api/controllers/jwt-helper.ts
+++ src/api/controllers/jwt-helper.ts
@@ -2,23 +2,71 @@ import express = require("express");
 import httpStatus = require("http-status");
 import jwt = require("jsonwebtoken");
 export class JwtHelper {
-    public async verifyAsyncToken(request: any, response: express.Response) {
+    public async verifyAccessToken(request: any, response: express.Response, isResponseNeeded: boolean = false) {
         try {
             const authHeader: string | undefined = request.headers["authorization"];
-            if( !authHeader ) {
+            const authToken: any = authHeader && (authHeader.split(' '))[1];
+            if( !authToken || authToken === undefined ) {
                 throw "The auth token is missing!";
             }
-            const authToken: string | undefined = (authHeader.split(' '))[2];
-            jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET as string, (err, payload) => {
+            jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET as string, (err: any, payload: any) => {
                 if(err) {
                     response.status(httpStatus.UNAUTHORIZED).send("The user is unauthorized!");
                 } else {
                     request.payload = payload;
+                    if(isResponseNeeded) {
+                        response.status(httpStatus.OK).send();
+                    }
                 }
             });
         } catch(err) {
             console.log(err);
             response.status(httpStatus.UNAUTHORIZED).send("The user cannot be authorized!");
         }
+    }
+    public async getAccessFromRefreshToken(request: any, response: express.Response) {
+        try {
+            const requestBody = request.body;
+            const refreshToken = requestBody.refreshToken;
+            if( !requestBody || !refreshToken ) {
+                throw "The refresh token is missing!";
+            }
+            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string, async (err: any, payload: any) => {
+                if(err) {
+                    response.status(httpStatus.UNAUTHORIZED).send("The user is unauthorized!");
+                } else {
+                    const accessToken = await this.signAccessToken(payload.aud, payload.role);
+                    response.status(httpStatus.OK).send({
+                        accessToken: accessToken,
+                        refreshToken: refreshToken,
+                        role: payload.role
+                    });
+                }
+            });
+        } catch(err) {
+            console.log(err);
+            response.status(httpStatus.UNAUTHORIZED).send("The user cannot be authorized!");
+        }
+    }
+
+    private async signAccessToken(userEmail: string, userRole: string) {
+        return new Promise((resolve, reject) => {
+            const payload = {
+                role: userRole // "vendor" or "customer"
+            };
+            const secret: string = process.env.ACCESS_TOKEN_SECRET as string;
+            const options = {
+                expiresIn: "10m",
+                issuer: "e-com.com",
+                audience: userEmail
+            };
+            jwt.sign(payload, secret, options, (err: any, token: any) => {
+                if(err) {
+                    console.log(err);
+                    reject("Internal Server Error!");
+                }
+                resolve(token);
+            });
+        });
     }
 }
--- src/api/routes/routes.ts
+++ src/api/routes/routes.ts
@@ -1,42 +1,31 @@
 import apiControllersFactory = require("../../controller-factory");
 import express = require("express");
-import httpStatus = require("http-status");
 
 export function routes(app: express.Express) {
     const { jwtHelper, vendorDataController, customerDataController, accountBalanceController } = apiControllersFactory.getApiControllers();
-    app.route("/").get( async (req: any, res: any) => {
-       await jwtHelper.verifyAsyncToken(req, res);
-       if(!res.headersSent && (!req.payload || !(req.payload.role !== "vendor" || req.payload.role !== "customer"))) {
-            res.status(httpStatus.BAD_REQUEST).send("Invalid input!");
-       } else if(!res.headersSent) {
-           if(req.payload.role === "vendor") {
-                res.redirect("/v0/vendor/get-my-items");
-            } else {
-                res.redirect("/v0/customer/shop");
-            }
-       }
-    });
+    app.route("/").get( async (req: any, res: any) => jwtHelper.verifyAccessToken(req, res, true));
+    app.route("/verify-refresh-token").post((req, res) => jwtHelper.getAccessFromRefreshToken(req, res));
     app.route("/v0/vendor/item").post( async (req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             vendorDataController.pushItemData(req, res);
         }
     })
     .patch( async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             vendorDataController.editItemData(req, res);
         }
     })
     .delete(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             vendorDataController.deleteItem(req, res);
         }
     });
     app.route("/v0/vendor/get-my-items").get( async(req: any, res: any) => {
         if(!req.payload) {
-            await jwtHelper.verifyAsyncToken(req, res);
+            await jwtHelper.verifyAccessToken(req, res);
         }
         if(!res.headersSent) {
             vendorDataController.fetchVendorItems(req, res);
@@ -44,50 +33,50 @@ export function routes(app: express.Express) {
     });
     app.route("/v0/customer/shop").get( async(req: any, res: any) => {
         if(!req.payload) {
-            await jwtHelper.verifyAsyncToken(req, res);
+            await jwtHelper.verifyAccessToken(req, res);
         }
         if(!res.headersSent) {
             customerDataController.fetchItems(req, res);
         }
     });
     app.route("/v0/customer/item").post(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             customerDataController.buyItem(req, res);
         }
     })
     .get(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             customerDataController.purchasedItems(req, res);
         }
     });
     app.route("/v0/customer/watchlist").post(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             customerDataController.addToWatchlist(req, res);
         }
     })
     .get(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             customerDataController.fetchWatchlist(req, res);
         }
     })
     .delete(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             customerDataController.deleteWatchlistItem(req, res);
         }
     });
     app.route("/v0/add-balance").post(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             accountBalanceController.addBalance(req, res);
         }
     });
     app.route("/v0/withdraw-balance").post(async(req, res) => {
-        await jwtHelper.verifyAsyncToken(req, res);
+        await jwtHelper.verifyAccessToken(req, res);
         if(!res.headersSent) {
             accountBalanceController.withdrawBalance(req, res);
         }
